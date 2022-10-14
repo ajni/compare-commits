9c95ba6fa18f32ce1587b3cb0e01d000f1b42c0f
Philip Chapin <phil.chapin@gmail.com>
2022-10-14 17:22:26 -0400

    Create skeleton for Python code, tests, and CI

---
+++ cc.py
@@ -0,0 +1,10 @@
+def main():
+    print(hello())
+
+
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    main()
---
+++ cc_test.py
@@ -0,0 +1,5 @@
+from cc import hello
+
+
+def test_hello():
+    assert hello() == "Hello World!"
